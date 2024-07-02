736a4f4b37e47dc85f1945fb2f5a3e2176fdb69b
Kbaltazar55555 <kenneth.baltazarv@gmail.com>
2024-07-02 11:25:35 -0400

    new update for apollo

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000', // Corrected from 'url' to 'uri'
+  cache: new InMemoryCache(),
+});
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>
 );
